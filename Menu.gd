6eb34421dfe10e1dd16658bd07271ea2f7d5bef1
nataliasou <natalia-souza@outlook.com>
2024-06-23 15:24:33 -0300

    Add loop de musica de fundo

--- Menu.gd
+++ Menu.gd
@@ -1,10 +1,10 @@
 extends Control
-
-func _on_ready():
-	$AudioStreamPlayer2D.play()
+@export var new_music: AudioStream
 	
 func _ready():
-	$AudioStreamPlayer2D.play()
+	var default_music = load("res://Trilha Sonora/Pense Em Mim (Karaoke).wav")
+	if MusicaFundo.music_player.stream != default_music:
+		MusicaFundo.play_music(default_music)
 	for button in get_tree().get_nodes_in_group("button"):
 		button.connect("pressed", self.on_button_pressed.bind(button))
 		button.connect("mouse_exited", self.mouse_interaction.bind(button, "exited"))
@@ -13,15 +13,17 @@ func _ready():
 func on_button_pressed(button: Button) -> void:
 	match button.name:
 		"Jogar":
+			MusicaFundo.stop_music()
+			var default_music = load("res://Trilha Sonora/E O Amor (Karaoke).WAV")
+			MusicaFundo.play_music(default_music)
 			get_tree().change_scene_to_file("res://scenes/main.tscn")
-			$AudioStreamPlayer2D.stop()
 		"Controles":
 			get_tree().change_scene_to_file("res://scenes/controles.tscn")
 		"Créditos":
 			get_tree().change_scene_to_file("res://scenes/creditos.tscn")
 		"Sair":
 			get_tree().quit()
-			$AudioStreamPlayer2D.stop()
+			MusicaFundo.stop_music()
 
 func mouse_interaction(button: Button, state: String) -> void:
 	match state:
